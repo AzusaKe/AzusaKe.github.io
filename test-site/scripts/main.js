8dbd3b604a1f75673bb34491c43fe190763cef82
Azusa_Ke <114424742+zhuozike@users.noreply.github.com>
2023-12-17 11:53:45 +0800

    Update main.js

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -2,7 +2,7 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === "images/20231115j9d2kg.png") {
     myImage.setAttribute("src", "images/逃离.png");
   } else {
     myImage.setAttribute("src", "images/20231115j9d2kg.png");
