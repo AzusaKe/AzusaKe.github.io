2ea158e6034b6dd237a4c8fad5745d4b807c4079
Azusa_Ke <114424742+AzusaKe@users.noreply.github.com>
2023-12-31 23:14:07 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
 window.onload = alert("欢迎来到我的个人网站")
-function updateTime() {
-      const timeElement = document.getElementById('time');
-      const now = new Date();
-      const hours = now.getHours().toString().padStart(2, '0');
-      const minutes = now.getMinutes().toString().padStart(2, '0');
-      const seconds = now.getSeconds().toString().padStart(2, '0');
-      timeElement.textContent = `${hours}:${minutes}:${seconds}`;
-    }
+function updateTime() 
+  {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+  }
 setInterval(updateTime, 1000);
